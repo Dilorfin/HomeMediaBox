78980e33103f0859af42515f5c568eb410824333
Dilorfin <pavel.kuybida@gmail.com>
2021-09-06 23:38:55 +0300

    Add probable solutions for 'is not a known element' error highlight

--- src/app/surfing/surfing.module.ts
+++ src/app/surfing/surfing.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SurfingRoutingModule } from './surfing-routing.module';
 import { SurfingComponent } from './surfing.component';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
 	declarations: [SurfingComponent],
 	imports: [
 		CommonModule,
-		SurfingRoutingModule
-	]
+		SurfingRoutingModule,
+		IonicModule //???
+	],
+	/*schemas: [CUSTOM_ELEMENTS_SCHEMA]*/
 })
 export class SurfingModule { }
