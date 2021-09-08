47ca78e60430c8f9161762ac7d180e6ff2d59306
Dilorfin <pavel.kuybida@gmail.com>
2021-09-08 22:51:06 +0300

    Add primitive using of video provider

--- src/app/_services/video.service.ts
+++ src/app/_services/video.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import DetailsModel from 'src/models/DetailsModel';
+import VideoFileModel from 'src/models/VideoFileModel';
 import VideoCdnProvider from 'src/providers/video/VideoCdn';
 import VideoProvider from 'src/providers/VideoProvider';
 
@@ -14,5 +16,14 @@ export class VideoService
 		this.videoProviders = [
 			new VideoCdnProvider()
 		];
+	}
+
+	getVideos(movieModel: DetailsModel): {title:string, videos:Promise<VideoFileModel[]>}[]
+	{
+		return this.videoProviders.map(provider=>{
+			const title = provider.getProviderTitle();
+			const videos = provider.getVideos(movieModel);
+			return {title, videos};
+		});
 	}
 }
--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import DetailsModel from 'src/models/DetailsModel';
 import ListModel from 'src/models/ListModel';
+import VideoFileModel from 'src/models/VideoFileModel';
 import { KnowledgeService } from '../_services/knowledge.service';
+import { VideoService } from '../_services/video.service';
 
 @Component({
 	selector: 'app-movie',
@@ -12,16 +14,26 @@ import { KnowledgeService } from '../_services/knowledge.service';
 export class MovieComponent implements OnInit
 {
 	movie: DetailsModel;
+	providers: {title:string, videos:VideoFileModel[]}[]
 
-	constructor(public router: Router, private knService: KnowledgeService)
+	constructor(public router: Router, private knService: KnowledgeService,
+		private videoService:VideoService)
 	{ }
 
 	ngOnInit()
 	{
 		this.movie = this.router.getCurrentNavigation().extras.state as DetailsModel;
+		this.providers = [];
+		
 		this.knService.getDetails(this.movie as unknown as ListModel)// ???
 			.then((value: DetailsModel)=>{
 				this.movie = value;
-			}); 
+				this.videoService.getVideos(value)
+					.forEach(provider => 
+						provider.videos.then(videos=>{
+							this.providers.push({title:provider.title, videos:videos});
+						})
+					)
+			});
 	}
 }
