13f88674985471e547cfe17627653066ba6c31a5
Dilorfin <pavel.kuybida@gmail.com>
2021-09-18 00:14:11 +0300

    Refactor a bit

--- src/app/movie/videos/videos.component.ts
+++ src/app/movie/videos/videos.component.ts
@@ -23,18 +23,19 @@ export class VideosComponent implements OnInit, OnChanges
 {
 	@Input() movie: DetailsModel;
 
-	currentFilter: {
-		quality: number,
-		translation: string,
-		season: string
-	};
 	filters: Record<string, {
 		qualities: number[],
 		translations: string[],
 		seasons: string[]
 	}> = {};
 
-	currentProvider: { title: string, videos: VideoFileModel[] };
+	currentProvider: {
+		title: string,
+		videos: VideoFileModel[],
+		quality: number,
+		translation: string,
+		season: string
+	};
 	providers: { title: string, videos: VideoFileModel[] }[];
 
 	constructor(private videoService: VideoService) 
@@ -58,8 +59,14 @@ export class VideosComponent implements OnInit, OnChanges
 					}
 					this.providers.push(value);
 					return value;
-				}).then(() =>
+				}).then((provider) =>
 				{
+					this.filters[provider.title] = {
+						qualities: filterUnique(provider.videos.map(video => video.quality)),
+						seasons: filterUnique(provider.videos.map(video => video.season_id)),
+						translations: filterUnique(provider.videos.map(video => video.voice_title))
+					};
+
 					this.setDefaultFilter();
 					this.filterVideos();
 				})
@@ -77,23 +84,15 @@ export class VideosComponent implements OnInit, OnChanges
 	setFilterProperty(property: string, event: any)
 	{
 		const value = event.detail.value;
-		this.currentFilter[property] = value;
+		this.currentProvider[property] = value;
 		this.filterVideos();
 	}
 
 	setDefaultFilter()
 	{
-		this.filters[this.currentProvider.title] = {
-			qualities: filterUnique(this.currentProvider.videos.map(video => video.quality)),
-			seasons: filterUnique(this.currentProvider.videos.map(video => video.season_id)),
-			translations: filterUnique(this.currentProvider.videos.map(video => video.voice_title))
-		};
-
-		this.currentFilter = {
-			quality: this.filters[this.currentProvider.title].qualities[0],
-			translation: this.filters[this.currentProvider.title].translations[0],
-			season: this.filters[this.currentProvider.title].seasons.length > 0 ? this.filters[this.currentProvider.title].seasons[0] : null
-		}
+		this.currentProvider.quality = this.filters[this.currentProvider.title].qualities[0];
+		this.currentProvider.translation = this.filters[this.currentProvider.title].translations[0];
+		this.currentProvider.season = this.filters[this.currentProvider.title].seasons.length > 0 ? this.filters[this.currentProvider.title].seasons[0] : null
 	}
 
 	filterVideos()
@@ -101,9 +100,9 @@ export class VideosComponent implements OnInit, OnChanges
 		const provider = this.providers.find((pr) => pr.title == this.currentProvider.title);
 		this.currentProvider.videos = provider.videos.filter(videoModel =>
 		{
-			return videoModel.voice_title == this.currentFilter.translation
-				&& videoModel.quality == this.currentFilter.quality
-				&& videoModel.season_id == this.currentFilter.season;
+			return videoModel.voice_title == this.currentProvider.translation
+				&& videoModel.quality == this.currentProvider.quality
+				&& videoModel.season_id == this.currentProvider.season;
 		}).sort((a, b) => a.episode_id - b.episode_id);
 	}
 }
