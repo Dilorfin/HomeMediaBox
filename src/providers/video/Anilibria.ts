5e4f466cbf51a4a14515af0fbb157505419fbee6
Dilorfin <pavel.kuybida@gmail.com>
2021-09-22 23:33:00 +0300

    Refactor Anilibria provider

--- src/providers/video/Anilibria.ts
+++ src/providers/video/Anilibria.ts
@@ -10,8 +10,6 @@ export default class AnilibriaProvider implements VideoProvider
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0'
 	};
 
-	constructor() { }
-
 	public getProviderTitle(): string
 	{
 		return "Anilibria";
@@ -19,75 +17,73 @@ export default class AnilibriaProvider implements VideoProvider
 
 	public async getVideos(movieModel: DetailsModel): Promise<VideoFileModel[]>
 	{
-		const kitsu_url: string = `https://kitsu.io/api/edge/anime?filter[text]=${movieModel.original_title}`;
+		return await this.requestForEngJpTitle(movieModel).then((title) =>
+		{
+			const url = `https://api.anilibria.tv/v2/searchTitles?search=${title}`;
+			return fetch(url, { headers: this.headers })
+				.then((response: Response) => response.json())
+				.then((responseJson) =>
+				{
+					var videos: VideoFileModel[] = [];
 
-		return await fetch(kitsu_url, {
-			headers: this.headers
-		})
-			.then((response: Response) =>
-			{
-				return response.json();
-			})
-			.then((responseJson) =>
-			{
-				return responseJson.data.filter(d => d.type == 'anime')[0].attributes.canonicalTitle;
-			}).then((title) =>
-			{
-				const url = `https://api.anilibria.tv/v2/searchTitles?search=${title}`;
-				return fetch(url, {
-					headers: this.headers
-				})
-					.then((response: Response) =>
+					for (const item of responseJson)
 					{
-						return response.json();
-					})
-					.then((responseJson) =>
-					{
-						var videos: VideoFileModel[] = [];
-
-						for (const item of responseJson)
+						for (const key in item.player.playlist)
 						{
-							for (const key in item.player.playlist)
+							const ser = item.player.playlist[key];
+
+							if (ser.hls.fhd)
 							{
-								const ser = item.player.playlist[key];
+								videos.push(this.createVideoFileModel(item, ser, 'fhd'));
+							}
 
-								if (ser.hls.fhd)
-								{
-									videos.push({
-										voice_title: item.team.voice.join(", "),
-										episode_id: Number(ser.serie),
-										season_id: item.names.ru,
-										quality: 1080,
-										url: `https://${item.player.host}${ser.hls.fhd}`,
-									});
-								}
+							if (ser.hls.hd)
+							{
+								videos.push(this.createVideoFileModel(item, ser, 'hd'));
+							}
 
-								if (ser.hls.hd)
-								{
-									videos.push({
-										voice_title: item.team.voice.join(", "),
-										episode_id: Number(ser.serie),
-										season_id: item.names.ru,
-										quality: 720,
-										url: `https://${item.player.host}${ser.hls.hd}`,
-									});
-								}
-
-								if (ser.hls.sd)
-								{
-									videos.push({
-										voice_title: item.team.voice.join(", "),
-										episode_id: Number(ser.serie),
-										season_id: item.names.ru,
-										quality: 480,
-										url: `https://${item.player.host}${ser.hls.sd}`,
-									});
-								}
+							if (ser.hls.sd)
+							{
+								videos.push(this.createVideoFileModel(item, ser, 'sd'));
 							}
 						}
+					}
 
-						return videos;
-					});
+					return videos;
+				});
 			});
+	}
+
+	private requestForEngJpTitle(movieModel: DetailsModel): Promise<string>
+	{
+		const kitsu_url: string = `https://kitsu.io/api/edge/anime?filter[text]=${movieModel.original_title}`;
+		return fetch(kitsu_url, { headers: this.headers })
+			.then((response: Response) => response.json())
+			.then((responseJson) =>
+			{
+				const animes = responseJson.data.filter(d => d.type == 'anime');
+				return animes[0].attributes.canonicalTitle;
+			});
+	}
+
+	private createVideoFileModel(item, series, quality: 'fhd' | 'hd' | 'sd'): VideoFileModel
+	{
+		let quality_number = 240;
+		if (quality == 'hd')
+		{
+			quality_number = 720;
+		}
+		else if (quality == 'fhd')
+		{
+			quality_number = 1080;
+		}
+
+		return {
+			voice_title: item.team.voice.join(", "),
+			episode_id: Number(series.serie),
+			season_id: item.names.ru,
+			quality: quality_number,
+			url: `https://${item.player.host}${series.hls[quality]}`,
+		};
 	}
 };
