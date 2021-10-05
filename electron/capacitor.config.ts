e6d59e074f10fb5bad2ec3d739c98df3674b389a
Dilorfin <pavel.kuybida@gmail.com>
2021-10-05 20:00:21 +0300

    Add electron target

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { ElectronCapacitorConfig } from '@capacitor-community/electron';
+
+const config: ElectronCapacitorConfig  = {
+  appId: 'com.dilorfin.homemediabox',
+  appName: 'Home Media Box',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
