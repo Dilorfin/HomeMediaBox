9d4b84619d5bb7e3da2c53abf68692bb77e7b8ce
Dilorfin <pavel.kuybida@gmail.com>
2021-10-15 21:02:53 +0300

    Add more categories for TMDB

--- src/providers/knowledge/TMDB.ts
+++ src/providers/knowledge/TMDB.ts
@@ -37,7 +37,9 @@ export default class TMDB implements KnowledgeProvider
 	{
 		return [
 			MovieCategory.Film,
-			MovieCategory.Series
+			MovieCategory.Series,
+			MovieCategory.Cartoon,
+			MovieCategory.AnimatedSeries
 		];
 	}
 
@@ -53,9 +55,29 @@ export default class TMDB implements KnowledgeProvider
 			}
 		}
 
-		const media_type: 'tv' | 'movie' = category == MovieCategory.Film ? 'movie' : 'tv';
+		let url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB.apiKey}&language=${TMDB.locale}`;
+		let media_type:'tv'|'movie' = 'movie';
+		if (category == MovieCategory.Film)
+		{
+			url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB.apiKey}&language=${TMDB.locale}&without_genres=16`;
+			media_type = 'movie';
+		}
+		else if (category == MovieCategory.Cartoon)
+		{
+			url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB.apiKey}&language=${TMDB.locale}&with_genres=16`;
+			media_type = 'movie';
+		}
+		else if (category == MovieCategory.AnimatedSeries)
+		{
+			url = `https://api.themoviedb.org/3/discover/tv?api_key=${TMDB.apiKey}&language=${TMDB.locale}&with_genres=16`;
+			media_type = 'tv';
+		}
+		else if (category == MovieCategory.Series)
+		{
+			url = `https://api.themoviedb.org/3/discover/tv?api_key=${TMDB.apiKey}&language=${TMDB.locale}&without_genres=16`;
+			media_type = 'tv';
+		}
 
-		const url = `https://api.themoviedb.org/3/${media_type}/popular?api_key=${TMDB.apiKey}&language=${TMDB.locale}&page=1`;
 		return TMDB.getJson<PaginationModel<ListModel>>(url)
 			.then((movies: PaginationModel<ListModel>) =>
 			{
