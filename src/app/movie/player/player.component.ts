d8a04572121110fec0824b9fc353fe9612db3e3d
Dilorfin <pavel.kuybida@gmail.com>
2021-12-22 23:30:07 +0200

    Add player with hls support

--- src/app/movie/player/player.component.ts
+++ src/app/movie/player/player.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import * as Plyr from 'plyr';
+import Hls from 'hls.js';
 
 @Component({
 	selector: 'app-player',
@@ -10,14 +12,29 @@ import { Location } from '@angular/common';
 export class PlayerComponent implements OnInit
 {
 	public file: string;
-
+	private player: Plyr;
+	
 	constructor(route: ActivatedRoute, private location: Location)
 	{
 		this.file = route.snapshot.queryParams['file'];
 	}
 
 	ngOnInit()
-	{ }
+	{ 
+		var video:any = document.querySelector('#player');
+		this.player = new Plyr(video);
+		
+		if (this.file.indexOf('.m3u8') <= 0 || !Hls.isSupported())
+		{
+			video.src = this.file;
+		}
+		else
+		{
+			const hls = new Hls();
+			hls.loadSource(this.file);
+			hls.attachMedia(video);
+		}
+	}
 
 	goBack(): void
 	{
