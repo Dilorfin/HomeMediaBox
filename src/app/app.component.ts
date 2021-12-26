a7113ecd99395eef211eba733653453c65efce11
Dilorfin <pavel.kuybida@gmail.com>
2021-12-26 21:18:28 +0200

    Add notification for no internet connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ConnectionStatus, Network } from '@capacitor/network';
+import { ToastController } from '@ionic/angular';
+
 import Utils from './utils';
 
 @Component({
@@ -8,8 +11,53 @@ import Utils from './utils';
 })
 export class AppComponent
 {
-	constructor() 
+	private noInternetToast?: HTMLIonToastElement = null;
+
+	constructor(public toastController: ToastController)
 	{
 		Utils.extendDefaultPrototypes();
+
+		Network.addListener('networkStatusChange', (status: ConnectionStatus) =>
+		{
+			if (!status.connected)
+			{
+				this.presentToastWithOptions();
+			}
+		});
+		Network.getStatus().then((status: ConnectionStatus) =>
+		{
+			if (!status.connected)
+			{
+				this.presentToastWithOptions();
+			}
+		});
+	}
+
+	async presentToastWithOptions() 
+	{
+		if (this.noInternetToast) return;
+
+		this.noInternetToast = await this.toastController.create({
+			message: 'No internet connection',
+			icon: 'wifi-outline',
+			position: 'top',
+			buttons: [
+				{
+					icon: 'refresh-outline',
+					text: 'Retry',
+					handler: async () =>
+					{
+						const status = await Network.getStatus();
+						return status.connected;
+					}
+				}
+			]
+		});
+		await this.noInternetToast.present();
+
+		this.noInternetToast.onDidDismiss().then(() =>
+		{
+			this.noInternetToast = null;
+		});
 	}
 }
